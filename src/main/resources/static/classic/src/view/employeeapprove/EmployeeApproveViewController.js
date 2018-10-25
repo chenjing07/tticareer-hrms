449e4f1ed10c804895831275c14e7711f88fa4bb
dong <821250791@qq.com>
2018-10-25 11:45:51 +0800

    load employee

--- src/main/resources/static/classic/src/view/employeeapprove/EmployeeApproveViewController.js
+++ src/main/resources/static/classic/src/view/employeeapprove/EmployeeApproveViewController.js
@@ -50,6 +50,8 @@ Ext.define('Admin.view.employeeapprove.EmployeeApproveViewController', {
 		var grid = button.up('gridpanel');
 		var win=button.up('window');
 	    var values=win.down('form').getValues();//获得form数    
+	    var store1	= Ext.data.StoreManager.lookup('employeeApproveStore');
+	    var store2	= Ext.data.StoreManager.lookup('employeeGridStore');
 		Ext.Ajax.request(
 			{url:'/employee/approvePass', 
 			 method:'post', 
@@ -59,7 +61,9 @@ Ext.define('Admin.view.employeeapprove.EmployeeApproveViewController', {
 				//console.log(json);
 				if (json.data) {
 					Ext.Msg.alert('操作成功', json.msg, function() {
-						grid.getStore().reload();
+						//grid.getStore().reload();
+						store1.load();
+						store2.load();
 					});
 				} else {
 					Ext.Msg.alert('操作失败', json.msg);
